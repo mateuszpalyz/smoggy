a78a9eb7bb0aa3bb25ecd153569311de5129b327
Mateusz Palyz <mateusz.palyz@netguru.pl>
2017-03-19 12:54:02 +0100

    update city component

--- App/Components/City.js
+++ App/Components/City.js
@@ -6,33 +6,63 @@ import {
   View,
   Image
 } from 'react-native';
+import PercentageCircle from './PercentageCircle';
 
 var styles = StyleSheet.create({
   container: {
     marginTop: 10
   },
   title: {
-    marginBottom: 40,
+    color: '#FCFCFC',
     fontSize: 25,
+    marginBottom: 40,
     textAlign: 'center',
-    color: '#FCFCFC'
+  },
+  circle: {
+    alignSelf: 'center'
   },
   image: {
     borderRadius: 150,
     height: 300,
     width: 300,
-    alignSelf: 'center'
+  },
+  aqiOverlay: {
+    backgroundColor: 'rgba(0,0,0,0)'
+  },
+  mainInfo: {
+    color: '#884ADF',
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 40,
+    textAlign: 'center'
+  },
+  mainValue: {
+    color: '#884ADF',
+    fontSize: 70,
+    fontWeight: 'bold',
+    marginTop: 100,
+    textAlign: 'center'
   },
   info: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     marginTop: 40
   },
-  main: {
-    fontSize: 21,
-    color: '#FCFCFC'
+  infoBox: {
+    width: 120
   },
   secondary: {
-    fontSize: 17,
-    color: '#FCFCFC'
+    color: '#D7D7D7',
+    fontSize: 26,
+    fontWeight: '100',
+    marginLeft: 20,
+    marginRight: 20,
+    textAlign: 'center'
+  },
+  secondaryValue: {
+    color: '#884ADF',
+    fontSize: 19,
+    textAlign: 'center'
   }
 });
 
@@ -45,13 +75,31 @@ export default class City extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>{this.props.data.city.name}</Text>
-        <Image style={styles.image} source={{uri: mapUri}}/>
-        <View style={styles.info}>
-          <Text style={styles.main}>Air Quality Index 25</Text>
-          <Text style={styles.secondary}>PM2.5 2.5</Text>
-          <Text style={styles.secondary}>NO2 26</Text>
-          <Text style={styles.secondary}>CO 7</Text>
+        <View style={styles.circle}>
+          <PercentageCircle radius={151} percent={this.props.data.aqi/3} color={'#884ADF'} borderWidth={7}>
+            <Image style={styles.image} source={{uri: mapUri}}>
+              <View style={styles.aqiOverlay}>
+                <Text style={styles.mainInfo}>AQI</Text>
+                <Text style={styles.mainValue}>{this.props.data.aqi}</Text>
+              </View>
+            </Image>
+          </PercentageCircle>
         </View>
+        <View style={styles.info}>
+          <View style={styles.infoBox}>
+            <Text style={styles.secondaryValue}>{this.props.data.iaqi.pm25.v}</Text>
+            <Text style={styles.secondary}>PM2.5</Text>
+          </View>
+          <View style={styles.infoBox}>
+            <Text style={styles.secondaryValue}>{this.props.data.iaqi.no2.v}</Text>
+            <Text style={styles.secondary}>NO2</Text>
+          </View>
+          <View style={styles.infoBox}>
+            <Text style={styles.secondaryValue}>{this.props.data.iaqi.co.v}</Text>
+            <Text style={styles.secondary}>CO</Text>
+          </View>
+        </View>
+        <View style={styles.circle}></View>
         {showErr}
       </View>
     )
