dd4cf7e7b8340a6d382f4db2737574b2bb1a05c1
Mateusz Palyz <mateusz.palyz@netguru.pl>
2017-03-20 16:12:42 +0100

    add api for polution by coords

--- App/Utils/api.js
+++ App/Utils/api.js
@@ -3,6 +3,11 @@ var api = {
     city = city.toLowerCase().trim();
     var url = `https://api.waqi.info/feed/${city}/?token=bed973a1340c7ad855b8261bab7d34d4aaedc509`
     return fetch(url).then((res) => res.json());
+  },
+  getPolutionByCoordinates(lat, lng) {
+    var url = `https://api.waqi.info/feed/geo:${lat};${lng}/?token=bed973a1340c7ad855b8261bab7d34d4aaedc509`
+    console.log(url);
+    return fetch(url).then((res) => res.json());
   }
 };
 
